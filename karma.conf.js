310cbee91ec4599474801fff1bb6b147146bcac6
Rick <info@rickdegraaff.nl>
2017-03-07 12:50:30 +0100

    Switched to Chrome tester

--- karma.conf.js
+++ karma.conf.js
@@ -76,7 +76,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
